d2d5ed0d16e88932d9124122d5aca7eeba7c83ab
Bartosz <bartoszk00@o2.pl>
2024-04-06 21:37:41 +0200

    first frontend version

--- src/App.js
+++ src/App.js
@@ -1,13 +1,76 @@
-import { Container, Typography } from '@mui/material';
+import { Container, Typography, TextField, Grid, Slider, Checkbox } from '@mui/material';
+import { useState } from 'react';
+import redImage from './czerwony.jpg';
+import yellowImage from './zolty.jpg';
+import greenImage from './zielony.jpg';
+import "./App.css";
 
 function App() {
+  const [passwordLength, setPasswordLength] = useState(50);
+  const [image, setImage] = useState(redImage);
+
+  const handleSliderChange = (event, newValue) => {
+    setPasswordLength(newValue);
+ 
+    if (newValue < 10) {
+      setImage(redImage);
+    } else if (newValue >= 10 && newValue < 30) {
+      setImage(yellowImage);
+    } else {
+      setImage(greenImage);
+    }
+  };
+
   return (
     <Container>
-      <header>
-        <Typography variant="h4">
-          Generator przykładowych haseł
-        </Typography>
+      <header className={"App-header"} style={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+        <div style={{ flexGrow: 1, marginTop: '20px'}}>
+          <Typography variant="h3">
+            Generator przykładowych haseł
+          </Typography>
+          <Typography variant="subtitle1">
+            Wygeneruj silne i bezpieczne hasło dla swojego konta
+          </Typography>
+        </div>
       </header>
+      <Grid container alignItems="center" style={{ marginTop: '100px' }}>
+        <Grid item xs={6}>
+          <img src={image} alt="Obrazek" style={{ width: '300px', height: '300px' }} />
+        </Grid>
+        <Grid item xs={6}>
+          <Grid container direction="column" spacing={2}>
+            <Grid item>
+              <TextField 
+                label="" 
+                variant="outlined" 
+                style={{ width: '100%' }}
+              />
+            </Grid>
+            <Grid item>
+              <Typography variant="body1">Długość hasła: {passwordLength}</Typography>
+              <Slider
+                defaultValue={50}
+                value={passwordLength}
+                onChange={handleSliderChange}
+                aria-labelledby="discrete-slider"
+                valueLabelDisplay="auto"
+                step={1}
+                marks
+                min={1}
+                max={50}
+                style={{ width: '100%' }}
+              />
+            </Grid>
+            <Grid item>
+              <Typography variant="body1">Wybór znaków:</Typography>
+              <Checkbox defaultChecked /> abc
+              <Checkbox defaultChecked /> ABC
+              <Checkbox defaultChecked /> 123
+              <Checkbox defaultChecked /> !@#
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
