14c13e4fb6986f99d23a35f68e75b4c0e866b41f
Arkadiusz Wolski <arkadiusz.wolski5551@gmail.com>
2024-04-10 13:09:55 +0200

    feat: fetching password dictionaries from db

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require ("mongodb");
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3001;
 
 const PasswordGenerator = require("./PasswordGenerator");
+const uri = "mongodb+srv://mongopasswords:8SOpI4mJmAs8aWwy@passwordscluster.k0wzssr.mongodb.net/?retryWrites=true&w=majority&appName=PasswordsCluster";
+
+const client = new MongoClient(uri);
 
 const corsOptions = {
   origin: 'http://localhost:3000',
@@ -14,7 +18,28 @@ const corsOptions = {
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
 
+//db connection
 
+async function run() {
+  try {
+    const database = client.db('passwords');
+    const movies = database.collection('passwords-dictionary');
+    const passwords = await movies.find().toArray();
+
+    if(passwords.length === 0) {
+      return "";
+    }
+    
+    const randomIndex = Math.floor(Math.random() * passwords.length);
+    const randomPassword = passwords[randomIndex].password;
+    console.log(randomPassword);
+    return randomPassword;
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 //endpoints
 app.post('/generatePassword', (req, res) => {
@@ -33,7 +58,7 @@ app.post('/generatePassword', (req, res) => {
   );
 
   const responseData = {
-    password: generatedPassword
+    password: generatedPassword,
   }
 
   res.send(JSON.stringify(responseData));
